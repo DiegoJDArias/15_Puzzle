c4d3b60dde421b2095e90c147a81d61b4b1321c8
Diego J D Arias <diegojdarias@gmail.com>
2025-10-28 12:12:04 -0300

    Start Game on C++

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include "Game.h"
+
+/* ///////////////////////////////////////////////
+  Author Diego J D Arias - diegojdarias@gmail.com
+*///////////////////////////////////////////////
+
+
+int main() {
+
+    die::Game juego;
+    juego.run();
+    
+    return EXIT_SUCCESS;
+}
+
