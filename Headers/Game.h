26f2e20daeb06cc0954730a577f9e7467f97c0a2
Diego J D Arias <diegojdarias@gmail.com>
2025-10-28 15:14:27 -0300

    Implementation: Game Engine

--- Headers/Game.h
+++ Headers/Game.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Headers/SoundManager.h"
+#include "Headers/Cronometro.h"
+
+/* ///////////////////////////////////////////////
+  Author Diego J D Arias - diegojdarias@gmail.com
+*///////////////////////////////////////////////
+
+
+namespace die {
+    class Game {
+    public:
+        Game();
+        void run();
+
+    private:
+        const int width{ 390 };
+        const int height{ 320 };
+        int grid[6][6]{};
+        int contador{}, w{ 64 };
+        bool completado{ true };
+        bool resetTiempo{ true };
+        bool movimiento{ false };
+        bool activado{ false };
+
+        sf::RenderWindow app;
+        sf::Image icon;
+        sf::Texture t, menu;
+        sf::Sprite sprite[20], menus;
+        sf::Font texto;
+        sf::Text msj, score, tiempo, tomando;
+        sf::Color color;
+        die::Cronometro cronometro;
+        die::SoundManager sm;
+    };
+}
